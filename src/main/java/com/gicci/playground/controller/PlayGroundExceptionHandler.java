08a588e2fbee19b4f929de4ad04f981d384af131
Fernando Gicci Hernandes <fer.gicci@gmail.com>
2017-02-05 21:45:48 +0000

    update project
    add message source for internationalisation
    add exception handler controller advice.

---
+++ src/main/java/com/gicci/playground/controller/PlayGroundExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gicci.playground.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.gicci.playground.exception.RecordNotFound;
+
+@ControllerAdvice
+public class PlayGroundExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@ExceptionHandler({ RecordNotFound.class })
+	public ResponseEntity<Object> handleRecordNotFoundException(Exception ex, WebRequest request) {
+		String defaultMessage = messageSource.getMessage("Error.RecordNotFound", null, LocaleContextHolder.getLocale());
+		return new ResponseEntity<Object>(defaultMessage + ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+}
--- src/main/java/com/gicci/playground/exception/RecordNotFound.java
+++ src/main/java/com/gicci/playground/exception/RecordNotFound.java
@@ -1,27 +1,10 @@
 package com.gicci.playground.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecordNotFound extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	private static final String RECORD_NOT_FOUND = "Record Not Found. ";
-	
-	public RecordNotFound() {
-		super(RECORD_NOT_FOUND);
-	}
 	
 	public RecordNotFound(String message) {
-		super(RECORD_NOT_FOUND + message);
-	}
-	
-	public RecordNotFound(Throwable cause) {
-		super(cause);
-	}
-	
-	public RecordNotFound(String message, Throwable cause) {
-		super(RECORD_NOT_FOUND + message, cause);
+		super(message);
 	}
 }
